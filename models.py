514859c9c6abb9e242bb718fd5cec8dd7d4c0092
Mohamed Khalaf <matefkhalaf@gmail.com>
2020-06-19 03:48:54 +0200

    update app.py

--- models.py
+++ models.py
@@ -22,7 +22,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    # db.create_all()
+    db.create_all()
 
 
 '''
